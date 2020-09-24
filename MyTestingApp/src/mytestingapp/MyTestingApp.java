6f31a61690e884e97e5e2904ac62a1240899cec9
BermudezD33 <51296462+BermudezD33@users.noreply.github.com>
2020-09-24 10:55:06 -0600

    Donde se ejecutan los componentes

---
+++ MyTestingApp/src/mytestingapp/MyTestingApp.java
@@ -0,0 +1,16 @@
+package mytestingapp;
+
+/**
+ *
+ * @author Mega
+ */
+public class MyTestingApp {
+
+    public static void main(String[] args) {
+
+        MyForm myForm = new MyForm();
+        myForm.setVisible(true);
+
+    }
+
+}
